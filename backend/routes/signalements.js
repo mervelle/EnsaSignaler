a18da9d2ee70acf578551eb15ef685a6a0262e37
Mervelle <marwato1204@gmail.com>
2026-03-17 18:47:41 +0000

    finilized task 1 and task 2 from US2 load report list and updated frontend

--- backend/routes/signalements.js
+++ backend/routes/signalements.js
@@ -32,3 +32,24 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
+//router get
+// GET /signalements
+router.get('/', async (req, res) => {
+    console.log("GET /signalements HIT");
+
+    try {
+        const result = await pool.query(`
+            SELECT s.*, u.name 
+            FROM signalements s
+            JOIN users u ON s.user_id = u.id
+            ORDER BY s.created_at DESC
+        `);
+
+        res.json(result.rows);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Erreur serveur" });
+    }
+});
